e81cc04bd245e0ed2d8852b75c4ce2a1906c74ee
Frank Purcell <fxpurcell@gmail.com>
2014-04-18 17:16:30 -0700

    get buildout running

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import setuptools
 from setuptools import setup, find_packages
-setuptools.dist.Distribution(dict(setup_requires='Babel')) # for message_extractors line below (else warnings / errors)
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
@@ -35,32 +33,6 @@ extras_require = dict(
 if sys.version_info[:2] < (2, 7):
     requires.extend(['argparse>=1.2.1', 'unittest2>=0.5.1'])
 
-setup(name='test',
-      version='0.0',
-      description='test',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='test',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = test:main
-      [console_scripts]
-      initialize_test_db = test.scripts.initializedb:main
-      """,
-      )
 
 setup(
     name='ott.service',
@@ -78,20 +50,20 @@ setup(
     dependency_links=('http://opentransittools.com',),
     license="Mozilla-derived (http://opentransittools.com)",
     url='http://opentransittools.com',
-    keywords='ott, otp, controller, transit',
+    keywords='ott, otp, services, transit',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    test_suite='tests',
     install_requires=requires,
     extras_require=extras_require,
     tests_require=requires,
-    test_suite="ott.controller",
+    test_suite="ott.service.tests",
     entry_points="""\
         [paste.app_factory]
         main = test:main
         [console_scripts]
-        initialize_test_db = test.scripts.initializedb:main
+        initialize_test_db = ott.service.scripts.initializedb:main
     """,
+        #initialize_test_db = test.scripts.initializedb:main
         #main = ott.service.pyramid.app:main
 )
