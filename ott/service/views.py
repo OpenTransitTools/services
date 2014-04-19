bdd9c8f23f9e7c8a4b8a2ba87e61bf564f5c8b9b
Frank Purcell <fxpurcell@gmail.com>
2014-04-19 11:33:29 -0700

    minor changes config/etc...

--- ott/service/views.py
+++ ott/service/views.py
@@ -22,18 +22,6 @@ def my_view(request):
     return {'one': one.name, 'project': 'test'}
 
 conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_test_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
+Danger. Danger Chris Robson.  Danger.
 """
 
