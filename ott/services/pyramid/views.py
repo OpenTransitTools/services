cf8e71b5311c77119c1db8d5a5a361d988ed9274
Frank Purcell <fxpurcell@gmail.com>
2014-05-02 20:09:47 -0700

    add null error checking...

--- ott/services/pyramid/views.py
+++ ott/services/pyramid/views.py
@@ -256,18 +256,23 @@ def stress(request):
 
 def dao_response(dao):
     ''' using a BaseDao object, send the data to a pyramid Reponse '''
+    if dao is None:
+        dao = data_not_found
     return json_response(json_data=dao.to_json(), status=dao.status_code)
 
 def json_response(json_data, mime='application/json', status=200):
     ''' @return Response() with content_type of 'application/json' '''
+    if json_data is None:
+        json_data = data_not_found.to_json()
     return Response(json_data, content_type=mime, status_int=status)
 
 def json_response_list(lst, mime='application/json', status=200):
     ''' @return Response() with content_type of 'application/json' '''
     json_data = []
     for l in lst:
-        jd = l.to_json()
-        json_data.append(jd)
+        if l:
+            jd = l.to_json()
+            json_data.append(jd)
     return json_response(json_data, mime, status)
 
 def rollback_session(session):
