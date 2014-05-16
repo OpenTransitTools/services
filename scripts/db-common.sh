04296b7596764bd1dc1266565c97e5afce982816
Frank Purcell <fxpurcell@gmail.com>
2014-05-16 12:19:39 -0700

    fix gtfs path for tm

--- scripts/db-common.sh
+++ scripts/db-common.sh
@@ -14,9 +14,8 @@ export OTT_SCHEMA=${OTT_SCHEMA:="ott"}
 export OTT_DUMP=${OTT_SCHEMA}.tar
 export OTT_MIN_SIZE=10000000
 export OTT_DUMPER=$BASEDIR/bin/db-dump.sh
-export GTFS_DOMAIN=${GTFS_DOMAIN:="localhost/tmws"}
-export GTFS_ZIP=http://${GTFS_DOMAIN}/schedule/gtfs.zip
-
+export GTFS_ZIP=http://localhost/tmws/schedule/gtfs.zip
+export GTFS_DOMAIN=${GTFS_DOMAIN:="localhost/trimet.org"}
 
 function drop_schema()
 {
