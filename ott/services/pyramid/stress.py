9e5c8b25ee4d0695040a94f5ab7dfe87cf422b96
Frank Purcell <fxpurcell@gmail.com>
2014-05-23 16:27:17 -0700

    more stress testing added...

--- ott/services/pyramid/stress.py
+++ ott/services/pyramid/stress.py
@@ -2,6 +2,7 @@
     Not to be run in production (so remove from views.py when not in use) 
 '''
 
+import datetime
 import time
 import logging
 log = logging.getLogger(__file__)
@@ -13,6 +14,9 @@ from sqlalchemy.orm.exc import NoResultFound
 from ott.otp_client.trip_planner import TripPlanner
 from ott.utils import html_utils
 
+from ott.data.dao.headsign_dao import StopHeadsignDao
+from ott.data.dao.stop_schedule_dao import StopScheduleDao
+
 from gtfsdb import StopTime
 
 from .app import DB
@@ -23,6 +27,7 @@ def do_view_config(cfg):
     cfg.add_route('stress_tp',       '/stresstp')
     cfg.add_route('stress_load_db',  '/stressload')
     cfg.add_route('stress_sched_db', '/stresssched')
+    cfg.add_route('stress_json',     '/stressjson')
 
 @view_config(route_name='stress', renderer='text/plain', http_cache=0)
 def stress(request):
@@ -60,10 +65,26 @@ def stress_sched_db(request):
     sid = html_utils.get_first_param(request, 'stop_id', "2")
     rid = html_utils.get_first_param(request, 'route_id', None)
     session = DB.session()
-    stimes = StopTime.get_departure_schedule(session=session, stop_id=sid, route_id=rid)
+    stop_times = StopTime.get_departure_schedule(session=session, stop_id=sid, route_id=rid)
     out = ""
-    for st in stimes:
-        out = out + st.departure_time + " "
+    for st in stop_times:
+        if st.is_boarding_stop():
+            out = out + st.departure_time + " "
     return Response(out, content_type='text/plain')
 
+@view_config(route_name='stress_json', renderer='json', http_cache=0)
+def stress_json(request):
+    sid = html_utils.get_first_param(request, 'stop_id', "2")
+    rid = html_utils.get_first_param(request, 'route_id', None)
+    session = DB.session()
+    stop_times = StopTime.get_departure_schedule(session=session, stop_id=sid, route_id=rid)
+    out = ""
+    json = []
+    for i, st in enumerate(stop_times):
+        out = out + st.departure_time + " "
+        id = StopHeadsignDao.unique_id(st)
+        now = datetime.datetime.now()
+        time = StopScheduleDao.make_stop_time(st, id, now, i+1)
+        json.append(time)
+    return Response(json, content_type='text/plain')
 
